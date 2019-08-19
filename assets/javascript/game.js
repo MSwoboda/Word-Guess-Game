8ab9fd1af52eade0693993e9549dc850813861fa
swoboda <michal.swoboda@outlook.com>
2019-08-19 00:28:57 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-var wordBank = ["Saturn"," Mars","Earth","Curiosity","Artemis","Apollo","Flare","Hubble","Rover","Moon"];
+var wordBank = ["saturn"," mars","earth","curiosity","artemis","apollo","flare","hubble","rover","moon"];
 
-
+var hideChar= "_";
 
 var wordDisplay = document.getElementById("headerDisp");
 var wordDisplay = document.getElementById("wordDisp");
@@ -10,7 +10,7 @@ var wordDisplay = document.getElementById("scoreDisp");
 
 var wordGuessGame= {
     word: "  ",
-    guessedString: "  ",
+    guessedString: "",
     Wins: 0,
     Guesses: 0,
     guessLetters: [],
@@ -19,6 +19,7 @@ var wordGuessGame= {
 
     newGame: function() {
         console.clear();
+        // Add New Game to log
         var d = new Date();
         var n = d.toLocaleTimeString();
 
@@ -29,10 +30,13 @@ var wordGuessGame= {
         this.word =  wordBank[randSeed];
 
         console.log("Index: " + randSeed);
-        console.log("Word " + this.word);
+        console.log("Word: " + this.word);
 
+        this.guessedString=hideChar.repeat(this.word.length);
+
+        console.log( this.guessedString);
         this.guessLetters= [];
-        this.Guesses=10;
+        this.Guesses=10 ;
 
         //Create guessedString placeholder
         //this.guessedString=guessedString + repeat(word.length);
@@ -45,28 +49,42 @@ var wordGuessGame= {
 
 };
 
-
+function writeLetters(params) {
+    
+}
 document.onkeydown = function(event) {
 //TODO: replace all event.key with  tolowerCase 
 var muhGuess=event.key.toLocaleLowerCase();
-console.log(event.key);
+
+//console.log(event.key);
 //Auto Start On Key Press OR Force start
 if (muhGuess==='`' || wordGuessGame.gameStarted===false) wordGuessGame.newGame();
 
-if ((!wordGuessGame.guessLetters.includes(event.key.toLocaleLowerCase())) && (event.keyCode >= 65 && event.keyCode <= 90)) {
+if ((!wordGuessGame.guessLetters.includes(muhGuess)) && (event.keyCode >= 65 && event.keyCode <= 90)) {
     
-    if (wordGuessGame.word.includes(event.key.toLocaleLowerCase())) {
-        console.log("YAS QUEEN");
+
+
+    if (wordGuessGame.word.includes(muhGuess)) {
+        
+        wordGuessGame.guessedString[wordGuessGame.word.indexOf(muhGuess)]= muhGuess;
+
+        console.log(wordGuessGame.word.indexOf(muhGuess));
+        console.log("yoh queen");
+
+
+        if (wordGuessGame.guessedString===wordGuessGame.word) {
+            score++
+
+        }
+
     } else {
-        wordGuessGame.guessLetters.push(event.key)
+        wordGuessGame.guessLetters.push(muhGuess)
+
+
         wordGuessGame.Guesses--;
         if (wordGuessGame.Guesses<=0) wordGuessGame.newGame();
 
     }
-
-   
-
-
 
 
 } 
@@ -76,8 +94,7 @@ if ((!wordGuessGame.guessLetters.includes(event.key.toLocaleLowerCase())) && (ev
 // } else {
     
 // }
-
-    wordDisplay.textContent = event.key;
+    wordDisp.textContent = wordGuessGame.guessedString;
     guessDisp.textContent = wordGuessGame.Guesses;
     wordDisplay.textContent = event.key;
     wordDisplay.textContent = event.key;
